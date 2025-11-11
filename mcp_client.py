7c3b9f80a489f080b8738ea835645c81643dcdd7
Hannah Kendall <hbkendall@outlook.com>
2025-11-10 18:43:16 -0800

    checkpoint before I change even more things

--- mcp_client.py
+++ mcp_client.py
@@ -6,7 +6,7 @@ import asyncio
 
 client = MultiServerMCPClient(  
     {
-        "maemailth": {
+        "email": {
             "transport": "stdio",  # Local subprocess communication
             "command": "python",
             # Absolute path to your mcp_server.py file
@@ -17,16 +17,25 @@ client = MultiServerMCPClient(
 
 async def run_agent():
     tools = await client.get_tools()  
+    print("### TOOLS ###")
     print(tools)
+    print("#############")
     agent = create_agent(
         model,
         tools  
     )
-    prompt = input("Enter email prompt: ")
-    draft_response = await agent.ainvoke(
-        {"messages": [{"role": "user", "content": "draft an email!"}]}
-    )
-    print(draft_response)
+    # prompt = input("Enter email prompt: ")
+    # draft_response = await agent.ainvoke(
+    #     {
+    #         "messages": [
+    #             {
+    #                 "role": "user", 
+    #                 "content": "Call the tool provided to you regarding email sending! Do not ask for additional information before calling this tool."
+    #             }
+    #         ]
+    #     }
+    # )
+    # print(draft_response)
 
 if __name__ == '__main__':
     load_dotenv()
